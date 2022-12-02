3ce3bce9bfb54387d875436b473841503468d82b
Koen van Meijeren <koenvanmeijeren@gmail.com>
2022-12-02 15:24:17 +0100

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+use Src\MagicNumbers\IsAdultGuard;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+echo "Hello world!";
+$isAdultGuard = new IsAdultGuard();
---
+++ src/MagicNumbers/IsAdultGuard.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Src\MagicNumbers;
+
+final class IsAdultGuard {
+
+    public function __construct() {
+        if (2 > 3) {
+
+        }
+    }
+
+}
